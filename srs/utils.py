87e0aa9b956f2f2e506aca3336082f90f1204796
sudhanshu <bhanuupadhyay302448@gmail.com>
2024-07-03 17:56:23 +0530

    predict pipeline

--- srs/utils.py
+++ srs/utils.py
@@ -49,6 +49,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)
 
+# loading pickle file
 def load_object(file_path):
     try:
         with open(file_path,'rb') as file_obj:
